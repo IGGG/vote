321a43c3af9000eff969ed8561a1e30bea352dc0
Yuya ONUMA <yohnuma@gion-xy.net>
2015-10-17 20:35:15 +0900

    スマホ対応感を出していく

--- static/vote.js
+++ static/vote.js
@@ -1,11 +1,27 @@
 $(function() {
-    var socket = io.connect('/vote');
+    var socket = io.connect('/vote'),
+        $up_button = $('#up-button'),
+        $down_button = $('#down-button');
 
-    $('#up-button').click(function() {
+    $up_button.on('touchstart' , function() {
+        $up_button.addClass('fa-thumbs-up');
+        $up_button.removeClass('fa-thumbs-o-up');
         socket.emit('vote', '+1');
     });
 
-    $('#down-button').click(function() {
+    $up_button.on('touchend', function() {
+        $up_button.addClass('fa-thumbs-o-up');
+        $up_button.removeClass('fa-thumbs-up');
+    });
+
+    $down_button.on('touchstart', function() {
+        $down_button.addClass('fa-thumbs-down');
+        $down_button.removeClass('fa-thumbs-o-down');
         socket.emit('vote', '-1');
+    });
+
+    $down_button.on('touchend', function() {
+        $down_button.addClass('fa-thumbs-o-down');
+        $down_button.removeClass('fa-thumbs-down');
     });
 });
