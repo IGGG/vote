603ccddd3bd2345a68bc5b37119b964814ce582a
Yuya ONUMA <yohnuma@gion-xy.net>
2015-10-18 14:30:02 +0900

    おもてがわ

--- app.js
+++ app.js
@@ -19,17 +19,32 @@ var io = require('socket.io').listen(process.env.VOTE_PORT || 3000),
 
         return { come: come, leave: leave, get: get };
     })(),
-    n_votes = 0;
+    votes = (function() {
+        var n_good = 0,
+            n_poor = 0,
+            good = function() {
+                n_good += 1;
+                io_namespace.bulletin.emit('good', n_good);
+            },
+            poor = function() {
+                n_poor += 1;
+                io_namespace.bulletin.emit('poor', n_poor);
+            },
+            get = function() {
+                return { n_good: n_good, n_poor: n_poor };
+            };
+
+        return { good: good, poor: poor, get: get };
+    })();
 
 io_namespace.vote.on('connection', function(socket) {
     users.come();
     socket.on('vote', function(msg) {
         if (msg === '+1') {
-            n_votes += 1;
+            votes.good();
         } else if (msg === '-1') {
-            n_votes -= 1;
+            votes.poor();
         }
-        io_namespace.bulletin.emit('voted', n_votes);
     });
     socket.on('disconnect', function() {
         users.leave();
--- static/bulletin.js
+++ static/bulletin.js
@@ -1,13 +1,44 @@
 $(function() {
-    var $votes = $('#votes'),
-        $users = $('#users'),
+    var n_good = 0,
+        n_poor = 0,
+        n_users = 0,
+        $user = $('#user'),
+        $good = $('#good'),
+        $poor = $('#poor'),
+        $chart = $('#chart'),
+        chart = $chart.epoch({
+            type: 'pie',
+            data: [{ label: 'Good', value: 1 }, { label: 'Poor', value: 1 }],
+            inner: $chart.width() / 4
+        }),
         socket = io.connect('/bulletin');
 
+    var createChartData = function() {
+        return [
+            { label: 'Good', value: n_good },
+            { label: 'Poor', value: n_poor }
+        ];
+    };
+
+    var updateChart = function() {
+        $chart.update(createChartData());
+    };
+
+
     socket.on('#user-changed', function(msg) {
-        $users.text(msg);
+        n_users = parseInt(msg);
+        $user.text(n_users);
     });
 
-    socket.on('voted', function(msg) {
-        $votes.text(msg);
+    socket.on('good', function(msg) {
+        n_good = parseInt(msg);
+        $good.text(n_good);
+        updateChart();
+    });
+
+    socket.on('poor', function(msg) {
+        n_poor = parseInt(msg);
+        $poor.text(n_poor);
+        updateChart();
     });
 });
